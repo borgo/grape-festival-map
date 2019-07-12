d115c99deb2fb36e2c0c82763c363e39bd9db6cd
Boris Gombarcik <borgo@Boriss-MacBook-Pro.local>
2019-07-12 20:55:13 +0200

    Clean up

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,34 @@ function globalErrHandler(objects) {
     console.log("test api failed", objects);
 }
 
+
+
+//Menu loading based on width
+if (document.getElementById('app').clientWidth <= 600)  {
+  document.getElementById('menu').classList.add('closed');
+}
+else {
+  document.getElementById('menu').classList.add('open');
+  document.getElementById('menu-toggle').classList.add('is-active');
+}
+
+//Menu toggling
+document.getElementById('menu-toggle').onclick = function() {  
+  this.classList.toggle('is-active'); 
+  
+  if (document.getElementById('menu').classList.contains("open")) {
+    document.getElementById('menu').classList.remove('open'); 
+    document.getElementById('menu').classList.add('closed'); 
+  }
+  else {
+    document.getElementById('menu').classList.remove('closed'); 
+    document.getElementById('menu').classList.add('open'); 
+  }
+}
+
+
+
+
 async function fetchData(path) {
     return new Promise((resolve, reject) => {
         try {
