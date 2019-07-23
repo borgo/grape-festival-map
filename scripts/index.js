2716b685f2db10b53e51fe13b732fbd7a040e98b
Boris Gombarcik <boris.gombarcik@gmail.com>
2019-07-23 15:44:24 +0200

    Adding annotations

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,17 @@
 import { VctrApi } from "https://www.vectary.com/embed/viewer/v1/scripts/api/api.js";
 
-let api, data;
+let vctrApi;
+
+const appElement = document.getElementById('app');
+const menuElement = document.getElementById('menu');
+const toggleElement = document.getElementById('menu-toggle');
+
 
 function globalErrHandler(objects) {
     console.log("test api failed", objects);
 }
 
 //Menu loading based on width
-const appElement = document.getElementById('app');
-const menuElement = document.getElementById('menu');
 if (document.getElementById('app').clientWidth <= 600)  {
     menuElement.classList.add('closed');
 }
@@ -18,7 +21,6 @@ else {
 }
 
 //Menu toggling
-const toggleElement = document.getElementById('menu-toggle');
 toggleElement.onclick = function() {  
     this.classList.toggle('is-active'); 
     
@@ -43,34 +45,76 @@ for (var i = 0; i < items.length; i++) {
   items[i].addEventListener('click', highlightObjects, false);
 }
 
-async function fetchData(path) {
-    return new Promise((resolve, reject) => {
-        try {
-            fetch(path)
-                .then(file => file.json()
-                    .then(json => resolve(json)));
-        } catch (e) {
-            reject(e);
-        }
-    });
-}
+// async function fetchData(path) {
+//     return new Promise((resolve, reject) => {
+//         try {
+//             fetch(path)
+//                 .then(file => file.json()
+//                     .then(json => resolve(json)));
+//         } catch (e) {
+//             reject(e);
+//         }
+//     });
+// }
 
 async function run() {
-    console.log("Example API running..");
+  console.log("Example script running..");
 
-    try {
-        api = new VctrApi("g19", globalErrHandler);
+  function errHandler(err) {
+      console.log("API error", err);
+  }
 
-        await api.init();
+  async function onReady() {
+      console.log("API ready..");
 
-        data = await fetchData("../resource.json");
-                
-        console.log("Ready...");
-        console.log("All objects", await api.getObjects());
+      try {
+          console.log(await vctrApi.getObjects());
+          vctrApi.addAnnotation({                
+              name: "Grape Stage",                
+              objectName: "grape_stage"
+          });
+          vctrApi.addAnnotation({
+              name: "Orange stage",                
+              objectName: "orange_stage"
+          });
+          vctrApi.addAnnotation({
+            name: "Úschovňa",                
+            objectName: "uschovna"
+          });
+          vctrApi.addAnnotation({
+            name: "FM stage",
+            objectName: "fm_stage"
+          });
+          vctrApi.addAnnotation({
+            name: "Suzuki stage",
+            objectName: "suzuki_stage"
+          });
+          vctrApi.addAnnotation({
+            name: "Hlavný vstup",
+            objectName: "main_gate"
+          });
+          vctrApi.addAnnotation({
+            name: "Vstup stanové mesto",
+            objectName: "tent_gate"
+          });
 
-    } catch (e) {
-        globalErrHandler(e);
-    }
+          await vctrApi.enableAnnotations(true);
+
+      } catch (e) {
+          errHandler(e);
+      }
+
+  }
+
+  vctrApi = new VctrApi("g19", errHandler);
+  try {
+      await vctrApi.init();        
+      //addEventListeners();
+      onReady();
+      
+  } catch (e) {
+      errHandler(e);
+  }
 }
 
 run();
