250731a77a46d71fbe91e57cf4373f22693128d0
Marcel <marcel>
2019-08-02 17:37:12 +0200

    Annotations expanding

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,8 @@ const appElement = document.getElementById('app');
 const menuElement = document.getElementById('menu');
 const toggleElement = document.getElementById('menu-toggle');
 
+const annotationsMap = new Map();
+
 
 function globalErrHandler(objects) {
     console.log("test api failed", objects);
@@ -41,6 +43,9 @@ var items = document.querySelectorAll("#menu > ul.items > li > a");
 var highlightObjects = function() {
     var mesh = this.getAttribute("data-mesh");    
     vctrApi.highlightMeshByName(mesh, "#ffe81c", 0.2, true);
+    if (annotationsMap.has(mesh)) {
+        vctrApi.expandAnnotationsById([annotationsMap.get(mesh)], true, true);
+    }
 
     //close menu
     toggleElement.classList.toggle('is-active')
@@ -74,125 +79,144 @@ async function run() {
   async function onReady() {
       console.log("API ready..");
 
+      let annotation;
       try {
           console.log(await vctrApi.getObjects());
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
               name: "Grape Stage",
               objectName: "grape_stage"
           });
-          vctrApi.addAnnotation({
+          if (annotation) {
+              annotationsMap.set("grape_stage", annotation.id);
+          }
+          annotation = await vctrApi.addAnnotation({
               name: "Orange stage",
               objectName: "orange_stage"
           });
-          vctrApi.addAnnotation({
+          if (annotation) {
+            annotationsMap.set("orange_stage", annotation.id);
+        }
+          annotation = await vctrApi.addAnnotation({
             name: "Orange zóna",
             objectName: "orange_zona"
           });
-          vctrApi.addAnnotation({
+          if (annotation) {
+            annotationsMap.set("orange_zone", annotation.id);
+        }
+          annotation = await vctrApi.addAnnotation({
             name: "Úschovňa",
             objectName: "uschovna"
           });
-          vctrApi.addAnnotation({
+          if (annotation) {
+            annotationsMap.set("uschovna", annotation.id);
+        }
+          annotation = await vctrApi.addAnnotation({
             name: "Yeme tržnica",
             objectName: "trznica#10"
           });
-          vctrApi.addAnnotation({
+          if (annotation) {
+            annotationsMap.set("trznica#10", annotation.id);
+        }
+          annotation = await vctrApi.addAnnotation({
             name: "Gastro",
             objectName: "gastro"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "Očistec stage",
             objectName: "ocistec_stage"
           });
-          vctrApi.addAnnotation({
+          if (annotation) {
+            annotationsMap.set("ocistec_stage", annotation.id);
+        }
+          annotation = await vctrApi.addAnnotation({
             name: "Jameson",
             objectName: "jameson"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "365.bank stage",
             objectName: "365_stage"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "365.bank cafe",
             objectName: "365bank_cafe"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "Mastercard",
             objectName: "master_card"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "Suzuki stage",
             objectName: "suzuki_stage"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "Hlavný vstup",
             objectName: "main_entry"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "Vstup stanové mesto",
             objectName: "entry_tent_city"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "Chill village",
             objectName: "chill_village"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "Tent Inn",
             objectName: "tent_inn"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "Camp manager",
             objectName: "camp_manager"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "Tent Inn",
             objectName: "tent_inn"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "Nay stage",
             objectName: "nay_stage"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "Grape pavilon",
             objectName: "grape_pavilon"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "U rampa",
             objectName: "u_rampa"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "U rampa",
             objectName: "u_rampa"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "Stanové mestečko",
             objectName: "tents"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "Urban market",
             objectName: "urban_market_1"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "Merch",
             objectName: "merch"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "Kaufland zóna",
             objectName: "kaufland_zona"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "Mazagrande",
             objectName: "mazagrande"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "SSE zóna",
             objectName: "sse_zona"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "Pilsner Urquell",
             objectName: "pilsner"
           });
-          vctrApi.addAnnotation({
+          annotation = await vctrApi.addAnnotation({
             name: "Rádio_FM Urban market hangair",
             objectName: "radio_fm_urban_market_hangair"
           });
